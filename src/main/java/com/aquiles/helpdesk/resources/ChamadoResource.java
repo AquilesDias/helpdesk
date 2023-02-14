7921b1c63cc7fa562e2cb4467f87474ad1c73a8d
AquilesDias <kilesax@gmail.com>
2023-02-14 16:50:00 -0300

    Implementado o endpoint save para dominio Chamado

--- src/main/java/com/aquiles/helpdesk/resources/ChamadoResource.java
+++ src/main/java/com/aquiles/helpdesk/resources/ChamadoResource.java
@@ -5,11 +5,10 @@ import com.aquiles.helpdesk.domain.DTO.ChamadoDTO;
 import com.aquiles.helpdesk.service.ChamadoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,6 +18,8 @@ public class ChamadoResource {
 
     @Autowired
     private ChamadoService chamadoService;
+
+    /*****    REQUISIÇÕES GET     *****/
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id){
@@ -32,4 +33,13 @@ public class ChamadoResource {
        List<ChamadoDTO> chamadoDTOS = chamado.stream().map(x -> new ChamadoDTO(x)).collect(Collectors.toList());
        return ResponseEntity.ok().body(chamadoDTOS);
     }
+
+    /*****    REQUISIÇÕES POST     *****/
+    @PostMapping
+    public ResponseEntity create(@RequestBody ChamadoDTO chamadoDTO){
+        Chamado chamado = chamadoService.create(chamadoDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(chamado.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
 }
--- src/main/java/com/aquiles/helpdesk/service/ChamadoService.java
+++ src/main/java/com/aquiles/helpdesk/service/ChamadoService.java
@@ -1,6 +1,11 @@
 package com.aquiles.helpdesk.service;
 
 import com.aquiles.helpdesk.domain.Chamado;
+import com.aquiles.helpdesk.domain.Cliente;
+import com.aquiles.helpdesk.domain.DTO.ChamadoDTO;
+import com.aquiles.helpdesk.domain.Tecnico;
+import com.aquiles.helpdesk.domain.enums.Prioridade;
+import com.aquiles.helpdesk.domain.enums.Status;
 import com.aquiles.helpdesk.repositories.ChamadoRepository;
 import com.aquiles.helpdesk.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +20,12 @@ public class ChamadoService {
     @Autowired
     private ChamadoRepository chamadoRepository;
 
+    @Autowired
+    private TecnicoService tecnicoService;
+
+    @Autowired
+    private ClienteService clienteService;
+
     public Chamado findById(Integer id){
         Optional<Chamado> chamado = chamadoRepository.findById(id);
         return chamado.orElseThrow(() -> new ObjectNotFoundException("Chamado não encontrado! ID: " +id));
@@ -23,4 +34,27 @@ public class ChamadoService {
     public List<Chamado> findAll(){
         return chamadoRepository.findAll();
     }
+
+   public Chamado create(ChamadoDTO chamadoDTO){
+        return chamadoRepository.save(newChamado(chamadoDTO));
+   }
+
+   public Chamado newChamado(ChamadoDTO chamadoDTO){
+       Tecnico tecnico = tecnicoService.findById(chamadoDTO.getTecnico());
+       Cliente cliente = clienteService.findById(chamadoDTO.getCliente());
+
+       Chamado chamado = new Chamado();
+
+       if(chamadoDTO.getId() != null){
+           chamado.setId(chamadoDTO.getId());
+       }
+
+       chamado.setCliente(cliente);
+       chamado.setTecnico(tecnico);
+       chamado.setStatus(Status.toEnum(chamadoDTO.getStatus()));
+       chamado.setPrioridade(Prioridade.toEnum(chamadoDTO.getPrioridade()));
+       chamado.setTitulo(chamadoDTO.getTitulo());
+       chamado.setObservacoes(chamadoDTO.getObservacoes());
+       return chamado;
+   }
 }
